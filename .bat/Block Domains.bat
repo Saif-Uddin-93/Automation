effa0fa4433e2c674eb7d04d4da87ee9467ffda5
Saif Uddin <contact@saifuddin.uk>
2023-08-07 22:38:27 +0100

    corrected block domain script

--- .bat/Block Domains.bat
+++ .bat/Block Domains.bat
@@ -2,14 +2,27 @@
 
 set hostsPath=C:\Windows\System32\drivers\etc
 
+set max=5
+
 setlocal enabledelayedexpansion
+set blockedDomains[1]=google.com
+set blockedDomains[2]=google.co.uk
+set blockedDomains[3]=test.com
+set blockedDomains[4]=example.com
+set blockedDomains[5]=helloworld.com
+set /a index=1
 
-set blockedDomains[0]=google.com
-set blockedDomains[1]=google.co.uk
-
-for %%i in (0 1) do (
+:add
+for %%i in (%index%) do (
     set domain=!blockedDomains[%%i]!
     echo 0.0.0.0 !domain! >> "%hostsPath%\hosts"
+    set /a index+=1
+    if %index% lss %max% (GOTO:add)
 )
 
 endlocal
+
+pause
+
+
+
