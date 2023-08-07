ddb4e6fd1bc48ce5ad54473b367afc3639a84cfb
Saif Uddin <contact@saifuddin.uk>
2023-08-07 22:43:34 +0100

    powershell version to block domains

---
+++ PowerShell/Block Domains.ps1
@@ -0,0 +1,9 @@
+﻿$hostsPath="C:\Windows\System32\drivers\etc\hosts"
+$blockedDomains = @(
+"google.com"
+"google.co.uk"
+)
+
+foreach ($domain in $blockedDomains){
+    Add-Content -Path "$($hostsPath)" -Value "0.0.0.0 $($domain)" -Encoding ASCII
+}
