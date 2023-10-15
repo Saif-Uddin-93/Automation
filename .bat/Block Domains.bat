40a0d33ebbb61b62e15666fe19971c0967f59d1d
Saif Uddin <admin@saifuddin.uk>
2023-10-15 19:02:40 +0100

    more readable

--- .bat/Block Domains.bat
+++ .bat/Block Domains.bat
@@ -1,6 +1,6 @@
 @echo off
 
-set hostsPath=C:\Windows\System32\drivers\etc
+set hostsPath=C:\Windows\System32\drivers\etc\hosts
 
 setlocal enabledelayedexpansion
 
@@ -10,20 +10,19 @@ set blockedDomains[3]=test.com
 set blockedDomains[4]=example.com
 set blockedDomains[5]=helloworld.com
 
-set max=6
+set max=5
 set /a index=1
 
 :add
 set domain=!blockedDomains[%index%]!
 echo 0.0.0.0 !domain! >> "%hostsPath%"
-set /a index+=1
-::echo %index%
 if %index% lss %max% (GOTO:add)
+set /a index+=1
 
 endlocal
 
 pause
-::fsutil file createnew c:\temp\emptyfile 0
+::fsutil file createnew %hostsPath% 0
 
 
 
